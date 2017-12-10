723250429665301097bd034dd79664b7912b6fb5
claudiga <claudeclodi@hotmail.co.uk>
2017-12-10 05:19:26 +0000

    updated

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,7 @@
+var downPosition = 0;
+var upPosition = document.documentElement.scrollHeight;
+var intervalDown;
+var intervalUp;
 window.onload = function(){
     
     setInterval(function(){arrowBounce('leftarrow')}, 1000)
@@ -25,79 +29,59 @@ function arrowBounce(id){
 }
 
 function scrollDown(){
-
-//split 100% into 3
-
-var button = document.getElementsByTagName("BODY")[0];
-
-var currentScroll = document.documentElement.scrollTop;
-var entireHeight = document.documentElement.scrollHeight;
-
-var firstSection = entireHeight / 3;
-var secondSection = entireHeight / 2;
-var lastSection = entireHeight ;
-
-if(currentScroll < firstSection ){
-
-console.log("scroll to second")
-window.scroll(0,firstSection)
-console.log(firstSection)
-console.log(currentScroll)
+	
+       intervalDown = setInterval(function(){goDown()}, 5)
 
 
-}else if(currentScroll >=firstSection && currentScroll < lastSection ){
-
-console.log("scroll to last")
-window.scroll(0,lastSection)
-console.log()
-
-
-}else {
-alert("your already at the bottom sir are you crazy?!")
 }
 
+function goDown(){
 
+var entireHeight = document.documentElement.scrollHeight;
+var currentScroll = document.documentElement.scrollTop;
+var windowHeight = window.innerHeight;
+
+
+downPosition = downPosition+10;
+window.scroll(0,downPosition);
+
+if(currentScroll >= (entireHeight - (windowHeight)) ){
+	downPosition = 0;
+    clearInterval(intervalDown);
+
+}
 
 }
 
 function scrollUp(){
 
-var button = document.getElementsByTagName("BODY")[0];
+       intervalUp = setInterval(function(){goUp()}, 5)
 
-var currentScroll = document.documentElement.scrollTop;
+
+}
+
+function goUp(){
+
 var entireHeight = document.documentElement.scrollHeight;
-
-var firstSection = entireHeight / 3;
-var secondSection = entireHeight / 2;
-var lastSection = entireHeight;
+var currentScroll = document.documentElement.scrollTop;
+var windowHeight = window.innerHeight;
 
 
-if(currentScroll >=firstSection){
-console.log("scroll to second section")
-if(currentScroll ==firstSection){
-window.scroll(0,0);
-return;
-}
+upPosition = upPosition-10;
+window.scroll(0,upPosition);
+console.log(intervalUp)
 
-window.scroll(0,firstSection)
+if(currentScroll <= 0){
 
+upPosition = entireHeight;
+    clearInterval(intervalUp);
 
-
-}else if(currentScroll > 0){
-console.log("scroll to first section")
-window.scroll(0,0)
-}else{
-alert("you are already at the top")
-}
-
-
-
-
-
-
+	}
 
 
 }
+
+
 
 
 
